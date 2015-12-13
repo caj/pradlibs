7bde378e4a553d26fa0fc10cb48fe41c3942d635
Cody Jennings <cody.a.jennings@gmail.com>
2015-12-13 12:32:06 -0800

    Organizing

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
-require_relative './pradlibs.rb'
+require_relative 'src/prad_app.rb'
 
 run PradApp
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 require 'rspec'
 require 'rack/test'
 ENV['RACK_ENV'] = 'test'
-Dir[File.join(File.dirname(__FILE__), '..', '*.rb')].each { |f| require f }
+Dir[File.join(File.dirname(__FILE__), '..', 'src', '*.rb')].each { |f| require f }
