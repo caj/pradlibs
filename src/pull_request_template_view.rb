95c73c25ec2413d70d04b9165e37247b7987186a
Cody Jennings and Joshua Marker <pair+cody+jmarker@usertesting.com>
2016-03-21 16:39:53 -0700

    Module-ize template view

--- src/pull_request_template_view.rb
+++ src/pull_request_template_view.rb
@@ -1,20 +1,22 @@
-class PullRequestTemplateView < Mustache
+module PradLibs
+  class PullRequestTemplateView < Mustache
 
-  attr_accessor :implementation, :purpose, :trello_card_url
-  def initialize(pull_request_markdown_string)
-    parse_markdown_string(pull_request_markdown_string)
-  end
+    attr_accessor :implementation, :purpose, :trello_card_url
+    def initialize(pull_request_markdown_string)
+      parse_markdown_string(pull_request_markdown_string)
+    end
 
-  def has_trello_card?
-    @trello_card_url.present?
-  end
+    def has_trello_card?
+      @trello_card_url.present?
+    end
 
-  private
+    private
 
-  def parse_markdown_string(string)
-    match_data = string.match( /Purpose(.*)# Implementation(.*)# Trello Card(.*)# Additional/m ).to_a.map(&:strip)
-    @purpose = match_data[1]
-    @implementation = match_data[2]
-    @trello_card_url = match_data[3]
+    def parse_markdown_string(string)
+      match_data = string.match( /Purpose(.*)# Implementation(.*)# Trello Card(.*)# Additional/m ).to_a.map(&:strip)
+      @purpose = match_data[1]
+      @implementation = match_data[2]
+      @trello_card_url = match_data[3]
+    end
   end
 end
