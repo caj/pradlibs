3e97866f708c5c2b22f938552bf898219a1f0a33
Cody Jennings <cjennings@usertesting.com>
2016-03-27 10:57:28 -0700

    Make `Template`, `TemplatePool`, and `Dictionary` all comparable with `==`

--- src/dictionary.rb
+++ src/dictionary.rb
@@ -42,6 +42,11 @@ module PradLibs
       @words
     end
 
+    def == o
+      o.class == self.class &&
+        o.words == self.words
+    end
+
     def [] key
       keys = key.to_s.split('.').map(&:to_sym)
       ret = @words
