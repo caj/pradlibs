f661f63d5e5649d01e2868383930de2ec8fd5b0c
Cody Jennings <cjennings@usertesting.com>
2016-03-29 02:30:02 -0700

    Remove `PullRequestTemplateView`, refactor `PullRequestTemplateBuilder`

--- src/pull_request_template_builder.rb
+++ src/pull_request_template_builder.rb
@@ -1,27 +1,70 @@
 module PradLibs
-  class PullRequestTemplateBuilder
-    def initialize pool, dict = Dictionary.new
-      @dict = dict
+  class PullRequestTemplateBuilder < Builder
+    def initialize pr
+      @dict = Dictionary.new
+      @pool = PradLibs.load_template_file(File.join(PRADLIBS_TPLS, "pr_template/pr_template.yml"))
+      @pr = pr
     end
 
-    def create pr, args = {}
-      message = create_title pr
+    def create
+      message = create_title @pr
       {
-          response_type: :in_channel,
-          text: pr.html_url,
-          attachments: [
-              {
-                  fallback: message,
-                  author_name: pr.user.login,
-                  author_link: pr.user.html_url,
-                  author_icon: pr.user.avatar_url,
-                  title: message,
-                  title_link: pr.html_url,
-                  text: "##{pr.number}: #{pr.title}",
-                  thumb_url: get_image,
-              }
-          ]
+        response_type: :in_channel,
+        text: @pr.html_url,
+        attachments: [
+          {
+            fallback: message,
+            author_name: @pr.user.login,
+            author_link: @pr.user.html_url,
+            author_icon: @pr.user.avatar_url,
+            title: message,
+            title_link: @pr.html_url,
+            text: "##{@pr.number}: #{@pr.title}",
+          }
+        ]
       }
+    end
+
+    def create_title pull_request
+      @dict = @dict.merge({
+        prt: prt
+      })
+
+      super
+    end
+
+    def prt
+      {
+        purpose: purpose,
+        implementation: implementation,
+        has_trello_card?: has_trello_card?
+      }
+    end
+
+    def body
+      @body ||= @pr.body
+    end
+
+    def purpose
+      body.match(/.*\*Purpose\*([^\*]*)/)[1].strip
+    rescue
+      nil
+    end
+
+    def implementation
+      body.match(/.*\*Implementation\*([^\*]*)/)[1].strip
+    rescue
+      nil
+    end
+
+    def trello_card_url
+      body.match(/.*\*Trello Card:\*([^\*]*)/)[1].strip
+    rescue
+      nil
+    end
+
+    def has_trello_card?
+      !!trello_card_url
     end
   end
 end
