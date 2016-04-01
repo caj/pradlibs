60948b61ed39192eab416783f2426515055afaf9
Cody Jennings <cjennings@usertesting.com>
2016-04-01 15:35:48 -0700

    Fix: use correct method

--- src/pull_request_template_builder.rb
+++ src/pull_request_template_builder.rb
@@ -15,7 +15,7 @@ module PradLibs
         "response_type": :in_channel,
         "attachments": [
           {
-            "pretext": "#{@slack_params[:user_name]} requests code review for a PR in the #{@pl[:repo]} repository. <!here>",
+            "pretext": "#{@slack_params[:user_name]} requests code review for a PR in the #{get_pradlibs[:repo]} repository. <!here>",
             "fallback": "Purpose\n#{purpose}\n\nImplementation\n#{implementation}",
             "title": @pr.title,
             "title_link": @pr.html_url,
