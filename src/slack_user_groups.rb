39121200c44205d2433208a302f5ab9cdb5e5918
Duncan Leo <duncanleo97@gmail.com>
2016-09-07 17:39:49 -0700

    Use instance variable for Redis client instead

--- src/slack_user_groups.rb
+++ src/slack_user_groups.rb
@@ -7,15 +7,15 @@ module PradLibs
     def initialize
       redis_url = ENV['REDISTOGO_URL']
       if !redis_url.nil?
-        uri = URI.parse()
-        @@REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+        uri = URI.parse(redis_url)
+        @REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
       else
-        @@REDIS = Redis.new
+        @REDIS = Redis.new
       end
     end
 
     def get_slack_var(github_team_name)
-      @@REDIS.hget(REDIS_KEY, github_team_name)
+      @REDIS.hget(REDIS_KEY, github_team_name)
     end
   end
 end
